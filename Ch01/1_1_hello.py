4add437af6d9d6e0f9de32f42ba25a527b8c82cd
stillclsrn <stillclsrn@naver.com>
2021-04-26 17:03:03 +0900

    2021/04/26 파이썬 Ch01 수업예제

---
+++ Ch01/1_1_hello.py
@@ -0,0 +1,16 @@
+"""
+날짜:2021/04/26
+이름:김철학
+내용:파이썬 Hello World 출력하기
+"""
+print("Hellow World~")
+
+
+# 변수 (Variable) : 프로그래밍에서 데이터를 처리하기 위한 메모리 공강
+var1 = 1
+var2 = 2
+
+result = var1 + var2
+print('결과 :',result)
+
+
